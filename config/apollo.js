0e2ade2cbce007950f23502a939d077e99132532
Alfredo Romero Gonzalez <fredyglz.fr@gmail.com>
2025-07-06 21:07:21 -0600

    se actualiza la conexión al backend

--- config/apollo.js
+++ config/apollo.js
@@ -1,17 +1,19 @@
-import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
-import { setContext } from 'apollo-link-context';
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from "apollo-link-context";
+
+const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND;
 
 const httpLink = createHttpLink({
-  uri: 'https://limitless-beach-89250.herokuapp.com/',
+  uri: BACKEND_URL,
 });
 
 const authLink = setContext((_, { headers }) => {
   // Leer el storage almacenado
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
